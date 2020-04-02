24c5a4975ae7a57bf764c29ef15616299b39a492
livalterskjaer <liv.alterskjaer@icloud.com>
2020-04-02 16:14:57 +0200

    If-statement in loop() that we are hoping will cause the car to turn 45 degrees until there is no longer an obstacle.

--- smartcar.ino
+++ smartcar.ino
@@ -1,20 +1,34 @@
+#include <VL53L0X.h>
 #include <Smartcar.h>
+#include <Wire.h>
+VL53L0X sensor;
 
 BrushedMotor leftMotor(smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(smartcarlib::pins::v2::rightMotorPins);
-
 DifferentialControl control(leftMotor, rightMotor);
-
 SimpleCar car(control);
 
-void setup() {
-    car.setSpeed(100);
-    delay(1000);
-    car.setSpeed(-50);
-    delay(2000);
-    car.setSpeed(0);
+void setup(){
+  Serial.begin(9600);
+  Wire.begin();
+  car.setSpeed(50);
+  sensor.setTimeout(500);
+  if(!sensor.init()){
+    Serial.println("Failed to detect and initialize sensor!");
+    while(1){}
+  }
+  sensor.startContinuous();
 }
+void loop(){
+  Serial.println(sensor.readRangeContinuousMillimeters());
+  if(sensor.timeoutOccurred()) {
+  Serial.print("TIMEOUT");
+  }
 
-void loop() {
-
+  if(sensor.readRangeContinuousMillimeters() < 200) {
+    car.setSpeed(10);
+    car.setAngle(45);
+  } else {
+    car.setSpeed(50);
+  }
 }
