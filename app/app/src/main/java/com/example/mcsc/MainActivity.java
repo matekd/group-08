5229b10a0a3a8a43c53a3d0b1c798c8c55070981
livalterskjaer <liv.alterskjaer@icloud.com>
2020-04-28 14:53:39 +0200

    Buttons collected in one place of the document in order for easier re-use. Also: Logic for changing app UI state methods and onclick listeners.

--- app/app/src/main/java/com/example/mcsc/MainActivity.java
+++ app/app/src/main/java/com/example/mcsc/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.mcsc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.neurosky.connection.*;
+// import com.neurosky.connection.*; (TODO: Commented out for build to run)
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
@@ -12,6 +12,7 @@ import android.net.Uri; //for hyperlink in url
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 
@@ -29,7 +30,64 @@ public class MainActivity extends AppCompatActivity {
 
     OutputStream mmOutputStream;
 
-    // TODO Liv: Convert kotlin code to java and paste here, for switching app state
+    // Buttons for external connections
+    Button connectCar = (Button) findViewById(R.id.connectCarBtn);
+    Button connectHeadset = (Button) findViewById(R.id.connectHeadsetBtn);
+
+    // Buttons to switch app UI state
+    Button partial = (Button) findViewById(R.id.partialBtn);
+    Button total = (Button) findViewById(R.id.totalBtn);
+    Button manual = (Button) findViewById(R.id.manualBtn);
+
+    // Car control buttons
+    ImageButton forward = (ImageButton) findViewById(R.id.forwardBtn);
+    ImageButton backward = (ImageButton) findViewById(R.id.backwardBtn);
+    ImageButton left = (ImageButton) findViewById(R.id.leftBtn);
+    ImageButton right = (ImageButton) findViewById(R.id.rightBtn);
+
+    // EEG layout id for visibility regulation
+    RelativeLayout eegView = (RelativeLayout) findViewById(R.id.eegLayout);
+
+    public void hideControls() {
+        forward.setVisibility(View.GONE);
+        backward.setVisibility(View.GONE);
+        left.setVisibility(View.GONE);
+        right.setVisibility(View.GONE);
+    }
+
+    public void showControls() {
+        forward.setVisibility(View.VISIBLE);
+        backward.setVisibility(View.VISIBLE);
+        left.setVisibility(View.VISIBLE);
+        right.setVisibility(View.VISIBLE);
+    }
+
+    public void hideEeg() {eegView.setVisibility(View.GONE);}
+
+    public void showEeg() {eegView.setVisibility(View.VISIBLE);}
+
+    public void showTotalPartialBtns() {
+        manual.setVisibility(View.GONE);
+
+        total.setVisibility(View.VISIBLE);
+        partial.setVisibility(View.VISIBLE);
+    }
+
+    public void showTotalManualBtns() {
+        partial.setVisibility(View.GONE);
+
+        total.setVisibility(View.VISIBLE);
+        manual.setVisibility(View.VISIBLE);
+    }
+
+    public void showManualPartialBtns() {
+        total.setVisibility(View.GONE);
+
+        manual.setVisibility(View.VISIBLE);
+        partial.setVisibility(View.VISIBLE);
+    }
+
+
 
     // Method to take user to external github page in browser
     private void goToUrl(View view) {
@@ -45,9 +103,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myLabel = (TextView) findViewById(R.id.myLabel);
+        // Logic for changing app UI states
+        partial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTotalManualBtns();
+                showEeg();
+                showControls();
+            }
+        });
+        total.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showManualPartialBtns();
+                showEeg();
+                hideControls();
+            }
+        });
+        manual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTotalPartialBtns();
+                hideEeg();
+                showControls();
+            }
+        });
+
+        // myLabel = (TextView) findViewById(R.id.myLabel); (TODO: Commented out for build to run)
         
-        Button connectCar = (Button) findViewById(R.id.connectCarBtn);
         connectCar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -58,7 +141,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        ImageButton forward = (ImageButton) findViewById(R.id.forwardBtn);
         forward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,7 +150,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        ImageButton left = (ImageButton) findViewById(R.id.leftBtn);
         left.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -78,7 +159,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        ImageButton right = (ImageButton) findViewById(R.id.rightBtn);
         right.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,7 +168,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-        ImageButton backward = (ImageButton) findViewById(R.id.backwardBtn);
         backward.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
