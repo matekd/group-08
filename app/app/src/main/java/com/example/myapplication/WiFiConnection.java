d534104f9b85108f2d3becb9423e5047d5bfdb14
AnnanLao <55920573+AnnanLao@users.noreply.github.com>
2020-05-11 16:01:48 +0200

    Update WiFiConnection.java
    use interface for on click method (dont know if it works

--- app/app/src/main/java/com/example/myapplication/WiFiConnection.java
+++ app/app/src/main/java/com/example/myapplication/WiFiConnection.java
@@ -1,4 +1,5 @@
 package com.example.myapplication;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,7 +9,6 @@ import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,29 +21,9 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class WiFiConnection extends AppCompatActivity{
+public class WiFiConnection extends MainActivity implements View.OnClickListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        this.registerReceiver(this.mConnReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
-         //on click method for wifi connection
-        Button connectCarBtn = (Button) findViewById(R.id.connectCarBtn);
-        connectCarBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-
-                WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-                if(!wifi.isWifiEnabled()) {
-                    wifi.setWifiEnabled(true);
-                }
-
-                ConnectToNetworkWEP("smartcar", "123456789");
-            }
-        });
-    }
 
     //receive broadcast
     private BroadcastReceiver mConnReceiver = new BroadcastReceiver() {
@@ -67,8 +47,8 @@ public class WiFiConnection extends AppCompatActivity{
     {
         try {
             WifiConfiguration conf = new WifiConfiguration();
-            conf.SSID = "\"" + smartcar + "\"";   // Please note the quotes. String should contain SSID in quotes
-            conf.wepKeys[0] = 123456789;  //WEP password is in hex, we do not need to surround it with quotes.
+            conf.SSID = "\"" + "smartcar" + "\"";   // Please note the quotes. String should contain SSID in quotes
+            conf.wepKeys[0] = "123456789";  //WEP password is in hex, we do not need to surround it with quotes.
             conf.wepTxKeyIndex = 0;
             conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
             conf.allowedGroupCiphers.set(WifiConfiguration.GroupCipher.WEP40);
@@ -94,4 +74,27 @@ public class WiFiConnection extends AppCompatActivity{
             return false;
         }
     }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.registerReceiver(this.mConnReceiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        Button connectCarBtn = findViewById(R.id.connectCarBtn);
+        connectCarBtn.setOnClickListener(this) ;
+    }
+
+            @Override
+            public void onClick(View v) {
+                // Perform action on click
+
+                WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                if(!wifi.isWifiEnabled()) {
+                    wifi.setWifiEnabled(true);
+                }
+
+                ConnectToNetworkWEP("smartcar", "123456789");
+            }
 }
+
